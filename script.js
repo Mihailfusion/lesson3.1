6f4196481f18c57557c8dcc71782839af7622f5a
Mihailfusion <mihailfusion@gmail.com>
2019-03-04 22:36:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let str = ("урок-3 - был слишком легким");
+str = str[0].toUpperCase() + str.substring(1);
+document.write(str);
+str = str.split('-').join(' ');
+document.write("<br>" + str);
+console.log(str);
+str = str.split(" ");
+let res = str[6].replace('легким', 'легкоо');
+console.log(res);
+document.write("<br>" + res + "<br>");
+
+
+
+let arr = [20, 33, 1, "Человек", 2, 3];
+let count = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (typeof (arr[i]) === "number") {            
+            count = count + Math.pow(arr[i], 3);
+          
+    }
+} 
+count = Math.sqrt(count);
+            document.write(count);
+            console.log(count);
+
+
+
+
+let arg = prompt("Какой-то текст");
+function lorem() {
+    if (typeof arg === "string") {
+        arg.trim();
+        if (arg.length > 50) {
+            arg = arg.substr(0, 50) + '...';
+        }
+        // arg = arg.substr(0, 50) + '...';
+        document.write("<br><br>" + arg);
+    } else {
+        alert("Введите текст");
+    }
+    
+    
+}
+lorem();
